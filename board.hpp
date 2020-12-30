4e4e32b87d01ac0a9dec21b32cb2436029709358
edmondywen <edmondywen@gmail.com>
2020-12-29 19:38:14 -0800

    first commit. setting up basic file structure

---
+++ board.hpp
@@ -0,0 +1,6 @@
+#ifndef board_hpp
+#define board_hpp
+
+#include <stdio.h>
+
+#endif /* Board_hpp */
---
+++ game.hpp
@@ -0,0 +1,14 @@
+#ifndef game_hpp
+#define game_hpp
+
+#include <stdio.h>
+
+class Game {
+    public:
+    Game(int rows, int cols, int nMines);
+    ~Game();
+    
+    private:
+};
+
+#endif /* game_hpp */
